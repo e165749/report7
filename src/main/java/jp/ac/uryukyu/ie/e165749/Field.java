cb9aaa07482ea44ec50494ebec4c35684a68fa0d
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2017-02-06 21:34:01 +0900

    report7 Field   take2

--- src/main/java/jp/ac/uryukyu/ie/e165749/Field.java
+++ src/main/java/jp/ac/uryukyu/ie/e165749/Field.java
@@ -4,9 +4,9 @@ package jp.ac.uryukyu.ie.e165749;
  * Created by South on 2017/02/06.
  */
 public class Field {
-    public int width;  // 迷路の横の大きさ
-    public int height;  // 迷路の縦の大きさ
-    public byte[] data;  // 実際の迷路データ
-    public static final byte PATH = 0;  // 通路
+    public int width;  // フィールドの横の大きさ
+    public int height;  // フィールドの縦の大きさ
+    public byte[] data;  // 実際のフィールド
+    public static final byte PATH = 0;  // 通れるところ
     public static final byte WALL = 1;  // 壁
 }
