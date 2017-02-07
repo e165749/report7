c342b9fe5a2f107d225b25fa2a6319e8024ffbf0
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2017-02-07 19:04:25 +0900

    report7  DrawCanvasTest   take1

---
+++ src/test/java/jp/ac/uryukyu/ie/e165749/DrawCanvasTest.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e165749;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by South on 2017/02/07.
+ */
+public class DrawCanvasTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
